689e607a227ed4db9ccfe8d19f4608d8125dd370
exxman <exxmanster@gmail.com>
2018-08-11 04:01:01 -0700

    task_8

---
+++ task_8.py
@@ -0,0 +1,40 @@
+'''
+
+s = 'abc'
+s2 ="bla-bla-bla"
+s4 =bla-bla-blabla-bla-blabla-bla-blabla-bla-blabla-bla-blabla-bla-blabla-bla-blabla-bla-bla
+bla-bla-blabla-bla-blabla-bla-blabla-bla-blabla-bla-blabla-bla-blabla-bla-blabla-bla-bla
+
+#print(s4)
+
+#print (s4.count('a'))
+
+#print (s2.replace("-","*").upper())
+
+path = r'/home/exxman/tmp/'
+path_win = r"c:\temp\w32.fuking_virus.exe"
+#print(path)
+print(path_win)
+
+
+py = "Python"
+
+#print(py[-6],py[0])bla-bla-bl
+#print(py[:2])
+
+#print(s2[::2])
+
+
+
+timestamp = '18:13:42'
+timestamp_lst = (timestamp.split(':'))
+hours = int(timestamp[:2])
+minutes = int(timestamp[3:5])
+seconds = int(timestamp[6:])
+total_seconds = hours * (60*60) + minutes*60 + seconds
+print("Total seconds:",total_seconds)
+'''
+
+email = 'musk@spacex.com'
+email_name_idx = email.split('@'[0])
+email_name =
